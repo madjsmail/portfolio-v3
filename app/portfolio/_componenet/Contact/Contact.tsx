19b85bab89d02edb2ac40a500cac1e8dbaab5075
madjidsmail <madjidsmail1998.com>
2024-01-26 03:46:22 +0100

    refactor(app): add social media to the contact us page

--- app/portfolio/_componenet/Contact/Contact.tsx
+++ app/portfolio/_componenet/Contact/Contact.tsx
@@ -1,28 +1,52 @@
-import Badge from '@/app/_lib/components/Badge/badge'
-import ShineButton from '@/app/_lib/components/ShineButton/ShineButton'
-import React from 'react'
+import Badge from "@/app/_lib/components/Badge/badge";
+import ShineButton from "@/app/_lib/components/ShineButton/ShineButton";
+import React from "react";
+import { SiFacebook, SiGithub, SiInstagram, SiLinkedin, SiTwitter } from "react-icons/si";
 
 function Contact() {
   return (
     <section className="content-grid contact-grid">
-    <div className="container">
-      <Badge  type={'baby-powder'}>
-        Contact me
-      </Badge>
-      <h1>Contact me</h1>
-            <div className="text">
-            <h3 className="business">
-              you know the business
-            </h3>
-            <h3 className="code">
-              I know how to code
-            </h3>
-            </div>
-      <ShineButton variant="hover">Hire me</ShineButton>
 
-    </div>
-  </section>
-  )
+      <div className="container">
+        <Badge type={"baby-powder"}>Contact me</Badge>
+        <h1>Contact me</h1>
+        <div className="text">
+          <h3 className="business">You know the business</h3>
+          <h3 className="code">I know how to code</h3>
+        </div>
+        <ShineButton variant="hover">Hire me</ShineButton>
+      </div>
+      <div className="container">
+        <p>find me on </p>
+
+        <div id="social-test">
+
+          <ul className="social">
+            <li>
+              <SiFacebook />
+            </li>
+            <li>
+             
+              <SiTwitter />
+            </li>
+            <li>
+             
+              <SiInstagram />
+            </li>
+
+            <li>
+             
+              <SiGithub />
+            </li>
+            <li>
+             
+              <SiLinkedin />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
